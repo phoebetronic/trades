ee407b720077507e244f44611f2a380e78c17c07
xh3b4sd <xh3b4sd@gmail.com>
2022-07-04 21:34:07 +0200

    Update sto/tradesredis/key.go

--- sto/tradesredis/key.go
+++ sto/tradesredis/key.go
@@ -5,5 +5,5 @@ import (
 )
 
 func (r *Redis) Key() string {
-	return fmt.Sprintf("typ:tra|exc:%s|ass:%s", r.key.Exc(), r.key.Ass())
+	return fmt.Sprintf("tra:raw|exc:%s|ass:%s", r.key.Exc(), r.key.Ass())
 }
