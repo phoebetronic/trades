954ec2c395ca34d9f79137ab6ee955b8c79ed848
xh3b4sd <xh3b4sd@gmail.com>
2022-08-17 17:13:22 +0200

    provide iterator interface without prior full allocation

---
+++ typ/trades/framer.go
@@ -0,0 +1,90 @@
+package trades
+
+import (
+	"github.com/xh3b4sd/framer"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Framer struct {
+	fra *framer.Framer
+	tra *Trades
+}
+
+func (f *Framer) Last() bool {
+	return f.fra.Last()
+}
+
+func (f *Framer) Next() *Trades {
+	var fra framer.Frame
+	{
+		fra = f.fra.Next()
+	}
+
+	if fra.Empty() {
+		return nil
+	}
+
+	var tra *Trades
+	{
+		tra = f.next(fra)
+	}
+
+	return tra
+}
+
+func (f *Framer) next(fra framer.Frame) *Trades {
+	var tra *Trades
+	{
+		tra = &Trades{
+			EX: f.tra.EX,
+			AS: f.tra.AS,
+			ST: timestamppb.New(fra.Sta),
+			EN: timestamppb.New(fra.End),
+		}
+	}
+
+	var ind int
+	for i := 0; i < len(f.tra.TR); i++ {
+		{
+			ind = i
+		}
+
+		if !f.tra.TR[i].TS.AsTime().Before(fra.End) {
+			break
+		}
+		if f.tra.TR[i].TS.AsTime().Before(fra.Sta) {
+			continue
+		}
+
+		{
+			tra.TR = append(tra.TR, f.tra.TR[i])
+		}
+	}
+
+	// Once the next frame of trades got constructed, we remove the allocated
+	// trades from the source structure. This is to move trades around instead
+	// of duplicating them.
+	{
+		copy(f.tra.TR[0:], f.tra.TR[ind:])
+		for k, n := len(f.tra.TR)-ind, len(f.tra.TR); k < n; k++ {
+			f.tra.TR[k] = nil
+		}
+		f.tra.TR = f.tra.TR[:len(f.tra.TR)-ind]
+	}
+
+	// In the rare case that the last remaining trade happened at the exact end
+	// time of the very last frame, we simply empty the internal list of trades.
+	if len(f.tra.TR) == 1 && f.tra.TR[0].TS.AsTime().Equal(fra.End) {
+		f.tra.TR = nil
+	}
+
+	// Since the frame creation moves the trades window forward, we push the
+	// start time of the source structure to the end time of the constructured
+	// frame, because there are no trades for the constructured frame range
+	// anymore within the source structure.
+	{
+		f.tra.ST = timestamppb.New(fra.End)
+	}
+
+	return tra
+}
---
+++ typ/trades/framer_test.go
@@ -0,0 +1,122 @@
+package trades
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/xh3b4sd/framer"
+)
+
+func Test_Typ_Trades_Frame(t *testing.T) {
+	testCases := []struct {
+		tr *Trades
+		re []*Trades
+	}{
+		// Case 0
+		{
+			tr: &Trades{
+				EX: "ftx",
+				AS: "eth",
+				ST: newTim("2022-04-02T13:55:00Z"),
+				EN: newTim("2022-04-02T15:48:00Z"),
+				TR: []*Trade{
+					{PR: 120, LO: 3.5, TS: newTim("2022-04-02T13:55:00Z")},
+					{PR: 123, LO: 3.8, TS: newTim("2022-04-02T13:59:59Z")},
+					{PR: 137, SH: 2.3, TS: newTim("2022-04-02T14:00:00Z")},
+					{PR: 140, SH: 2.5, TS: newTim("2022-04-02T14:02:00Z")},
+					{PR: 110, SH: 3.1, TS: newTim("2022-04-02T14:22:00Z")},
+					{PR: 115, SH: 1.8, TS: newTim("2022-04-02T14:27:00Z")},
+					{PR: 117, SH: 2.9, TS: newTim("2022-04-02T14:59:59Z")},
+					{PR: 118, LO: 2.1, TS: newTim("2022-04-02T15:00:00Z")},
+					{PR: 125, LO: 4.2, TS: newTim("2022-04-02T15:11:00Z")},
+					{PR: 130, SH: 0.6, TS: newTim("2022-04-02T15:16:00Z")},
+					{PR: 128, LO: 1.5, TS: newTim("2022-04-02T15:33:00Z")},
+					{PR: 135, LO: 0.6, TS: newTim("2022-04-02T15:48:00Z")},
+				},
+			},
+			re: []*Trades{
+				{
+					EX: "ftx",
+					AS: "eth",
+					ST: newTim("2022-04-02T13:55:00Z"),
+					EN: newTim("2022-04-02T14:00:00Z"),
+					TR: []*Trade{
+						{PR: 120, LO: 3.5, TS: newTim("2022-04-02T13:55:00Z")},
+						{PR: 123, LO: 3.8, TS: newTim("2022-04-02T13:59:59Z")},
+					},
+				},
+				{
+					EX: "ftx",
+					AS: "eth",
+					ST: newTim("2022-04-02T14:00:00Z"),
+					EN: newTim("2022-04-02T15:00:00Z"),
+					TR: []*Trade{
+						{PR: 137, SH: 2.3, TS: newTim("2022-04-02T14:00:00Z")},
+						{PR: 140, SH: 2.5, TS: newTim("2022-04-02T14:02:00Z")},
+						{PR: 110, SH: 3.1, TS: newTim("2022-04-02T14:22:00Z")},
+						{PR: 115, SH: 1.8, TS: newTim("2022-04-02T14:27:00Z")},
+						{PR: 117, SH: 2.9, TS: newTim("2022-04-02T14:59:59Z")},
+					},
+				},
+				{
+					EX: "ftx",
+					AS: "eth",
+					ST: newTim("2022-04-02T15:00:00Z"),
+					EN: newTim("2022-04-02T15:48:00Z"),
+					TR: []*Trade{
+						{PR: 118, LO: 2.1, TS: newTim("2022-04-02T15:00:00Z")},
+						{PR: 125, LO: 4.2, TS: newTim("2022-04-02T15:11:00Z")},
+						{PR: 130, SH: 0.6, TS: newTim("2022-04-02T15:16:00Z")},
+						{PR: 128, LO: 1.5, TS: newTim("2022-04-02T15:33:00Z")},
+						{PR: 135, LO: 0.6, TS: newTim("2022-04-02T15:48:00Z")},
+					},
+				},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%03d", i), func(t *testing.T) {
+			var f *Framer
+			{
+				f = tc.tr.Frame(framer.Config{
+					Sta: tc.tr.ST.AsTime(),
+					End: tc.tr.EN.AsTime(),
+					Dur: time.Hour,
+				})
+			}
+
+			var re []*Trades
+			for !f.Last() {
+				re = append(re, f.Next())
+			}
+
+			if len(tc.tr.TR) != 0 {
+				t.Fatalf("len\n\n%s\n", cmp.Diff(len(tc.tr.TR), 0))
+			}
+
+			for i, r := range re {
+				if !reflect.DeepEqual(tc.re[i].EX, r.EX) {
+					t.Fatalf("EX index %d\n\n%s\n", i, cmp.Diff(tc.re[i].EX, r.EX))
+				}
+				if !reflect.DeepEqual(tc.re[i].AS, r.AS) {
+					t.Fatalf("AS index %d\n\n%s\n", i, cmp.Diff(tc.re[i].AS, r.AS))
+				}
+				if !reflect.DeepEqual(tc.re[i].ST, r.ST) {
+					t.Fatalf("ST index %d\n\n%s\n", i, cmp.Diff(tc.re[i].ST.AsTime(), r.ST.AsTime()))
+				}
+				if !reflect.DeepEqual(tc.re[i].EN, r.EN) {
+					t.Fatalf("EN index %d\n\n%s\n", i, cmp.Diff(tc.re[i].EN.AsTime(), r.EN.AsTime()))
+				}
+				for j := range r.TR {
+					if !reflect.DeepEqual(tc.re[i].TR[j], r.TR[j]) {
+						t.Fatalf("TR index %d/%d\n\n%s\n", i, j, fmt.Sprintf("%#v \n\n %#v", tc.re[i].TR[j], r.TR[j]))
+					}
+				}
+			}
+		})
+	}
+}
