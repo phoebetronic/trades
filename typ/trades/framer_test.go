49fc293771255b6a7fb2945d4f8e18231ae8ed94
xh3b4sd <xh3b4sd@gmail.com>
2022-08-19 13:43:43 +0200

    stop trades framer early on empty trades

--- typ/trades/framer_test.go
+++ typ/trades/framer_test.go
@@ -76,6 +76,67 @@ func Test_Typ_Trades_Frame(t *testing.T) {
 				},
 			},
 		},
+		// Case 1
+		{
+			tr: &Trades{
+				EX: "ftx",
+				AS: "eth",
+				ST: newTim("2022-04-02T13:55:00Z"),
+				EN: newTim("2022-07-04T18:24:00Z"),
+				TR: []*Trade{
+					{PR: 120, LO: 3.5, TS: newTim("2022-04-02T13:55:00Z")},
+					{PR: 123, LO: 3.8, TS: newTim("2022-04-02T13:59:59Z")},
+					{PR: 137, SH: 2.3, TS: newTim("2022-04-02T14:00:00Z")},
+					{PR: 140, SH: 2.5, TS: newTim("2022-04-02T14:02:00Z")},
+					{PR: 110, SH: 3.1, TS: newTim("2022-04-02T14:22:00Z")},
+					{PR: 115, SH: 1.8, TS: newTim("2022-04-02T14:27:00Z")},
+					{PR: 117, SH: 2.9, TS: newTim("2022-04-02T14:59:59Z")},
+					{PR: 118, LO: 2.1, TS: newTim("2022-04-02T15:00:00Z")},
+					{PR: 125, LO: 4.2, TS: newTim("2022-04-02T15:11:00Z")},
+					{PR: 130, SH: 0.6, TS: newTim("2022-04-02T15:16:00Z")},
+					{PR: 128, LO: 1.5, TS: newTim("2022-04-02T15:33:00Z")},
+					{PR: 135, LO: 0.6, TS: newTim("2022-04-02T15:48:00Z")},
+				},
+			},
+			re: []*Trades{
+				{
+					EX: "ftx",
+					AS: "eth",
+					ST: newTim("2022-04-02T13:55:00Z"),
+					EN: newTim("2022-04-02T14:00:00Z"),
+					TR: []*Trade{
+						{PR: 120, LO: 3.5, TS: newTim("2022-04-02T13:55:00Z")},
+						{PR: 123, LO: 3.8, TS: newTim("2022-04-02T13:59:59Z")},
+					},
+				},
+				{
+					EX: "ftx",
+					AS: "eth",
+					ST: newTim("2022-04-02T14:00:00Z"),
+					EN: newTim("2022-04-02T15:00:00Z"),
+					TR: []*Trade{
+						{PR: 137, SH: 2.3, TS: newTim("2022-04-02T14:00:00Z")},
+						{PR: 140, SH: 2.5, TS: newTim("2022-04-02T14:02:00Z")},
+						{PR: 110, SH: 3.1, TS: newTim("2022-04-02T14:22:00Z")},
+						{PR: 115, SH: 1.8, TS: newTim("2022-04-02T14:27:00Z")},
+						{PR: 117, SH: 2.9, TS: newTim("2022-04-02T14:59:59Z")},
+					},
+				},
+				{
+					EX: "ftx",
+					AS: "eth",
+					ST: newTim("2022-04-02T15:00:00Z"),
+					EN: newTim("2022-04-02T16:00:00Z"),
+					TR: []*Trade{
+						{PR: 118, LO: 2.1, TS: newTim("2022-04-02T15:00:00Z")},
+						{PR: 125, LO: 4.2, TS: newTim("2022-04-02T15:11:00Z")},
+						{PR: 130, SH: 0.6, TS: newTim("2022-04-02T15:16:00Z")},
+						{PR: 128, LO: 1.5, TS: newTim("2022-04-02T15:33:00Z")},
+						{PR: 135, LO: 0.6, TS: newTim("2022-04-02T15:48:00Z")},
+					},
+				},
+			},
+		},
 	}
 
 	for i, tc := range testCases {
@@ -95,7 +156,11 @@ func Test_Typ_Trades_Frame(t *testing.T) {
 			}
 
 			if len(tc.tr.TR) != 0 {
-				t.Fatalf("len\n\n%s\n", cmp.Diff(len(tc.tr.TR), 0))
+				t.Fatalf("len\n\n%s\n", cmp.Diff(0, len(tc.tr.TR)))
+			}
+
+			if len(re) != len(tc.re) {
+				t.Fatalf("len\n\n%s\n", cmp.Diff(len(re), len(tc.re)))
 			}
 
 			for i, r := range re {
